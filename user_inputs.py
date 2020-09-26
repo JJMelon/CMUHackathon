bcafa862554fcd7da30fce5d4087da3c417352e1
GITTyP1e <71911032+GITTyP1e@users.noreply.github.com>
2020-09-26 18:49:21 -0400

    Update user_inputs.py

--- user_inputs.py
+++ user_inputs.py
@@ -1,7 +1,51 @@
-name = input('Please give your name: ')
-level = 1
+import datetime
 
+class person:
+   person.name = ''
+   person.level = 1
+
+person.name
+   
 import main.py
 
-def getWorkout(level):
-   
+monday = giveWorkout(person.level)
+tuesday = giveWorkout(person.level)
+wednesday = giveWorkout(person.level)
+thursday = giveWorkout(person.level)
+friday = giveWorkout(person.level)
+saturday = giveWorkout(person.level)
+sunday = giveWorkout(person.level)
+ 
+weekday = datetime.datetime.today().weekday()
+
+dailyQuotes = '''
+I am Gunnery Sgt. Hartman, your senior drill instructor. From now on you will speak only when spoken to, and the first and last words out of your filthy sewers will be 'Sir.' Do you maggots understand that?
+
+
+
+
+
+'''
+
+quotesIfTired = '''
+Bullsh-t. It looks to me like the best part of you ran down the crack of your mama's ass and ended up as a brown stain on the mattress. I think you've been cheated!
+'''
+
+quotes = dailyQuotes.splitlines()
+
+for quotes in dailyQuotes.splitlines():
+   if (weekday == 0):
+      print('Quote of the day:', quote, f'Do your workout {location}','Workout:', monday)
+   elif (weekday == 1):
+      print('Quote of the day:', quote, f'Do your workout {location}','Workout:', monday)
+   elif (weekday == 2):
+      print('Quote of the day:', quote, f'Do your workout {location}','Workout:', monday)
+   elif (weekday == 3):
+      print('Quote of the day:', quote, f'Do your workout {location}','Workout:', monday)
+   elif (weekday == 4):
+      print('Quote of the day:', quote, f'Do your workout {location}','Workout:', monday)
+   elif (weekday == 5):
+      print('Quote of the day:', quote, f'Do your workout {location}','Workout:', monday)
+   elif (weekday == 6):
+      print('Quote of the day:', quote, f'Do your workout {location}','Workout:', monday)
+      
